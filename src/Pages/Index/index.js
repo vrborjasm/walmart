8235e8c9c2ebce088e3574ce30a192a38c51192d
Vicente Borjas <vrborjasm@gmail.com>
2022-01-16 10:53:33 -0300

    feat(Page.Index): Import ProductsList component

--- src/Pages/Index/index.js
+++ src/Pages/Index/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "../../components/common/NavBar";
+import ProductsList from "../../components/ProductsList";
 
 const Index = () => {
   return (
     <>
       <NavBar />
+      <ProductsList/>
     </>
   );
 };
